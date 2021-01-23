676f401edb164667c9f6491977a7c130500d35e2
onnohaldar <onno@haldar.nl>
2021-01-23 15:35:02 +0100

    refactor dir fuseki-server

--- src/lib/dgwnu-fuseki-server.ts
+++ src/lib/dgwnu-fuseki-server.ts
@@ -6,18 +6,23 @@
  * Node System Imports
  */
 import { execSync } from 'child_process';
+import { resolve } from 'path';
 
 /**
  * NPM Package Imports
  */
 
-const runServerCommand = '../fuseki-server/fuseki-server'; 
+const serverFolder = 'fuseki-server'; 
+const serverScript = 'fuseki-server'; 
 
 export function runServer(serverDefaults?: string) {
+    const runServerCommand = resolve(__dirname, '..', '..', serverFolder, serverScript);
 
     if (!serverDefaults) {
         serverDefaults = '--localhost --mem /dgwnu';
     }
 
-    return execSync(runServerCommand + ' ' + serverDefaults).toString();
+    console.log('runServerCommand: ', runServerCommand);
+
+    return execSync(`${runServerCommand} ${serverDefaults}`).toString();
 }
