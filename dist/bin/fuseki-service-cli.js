6c184560b204ec0e7f06205f5da0033de893b383
Amitava Haldar <onno.haldar@icloud.com>
2021-02-06 13:55:00 +0100

    refactor

--- dist/bin/fuseki-service-cli.js
+++ dist/bin/fuseki-service-cli.js
@@ -3,15 +3,36 @@
  * Run Fuseki
  */
 "use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    Object.defineProperty(o, k2, { enumerable: true, get: function() { return m[k]; } });
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 /**
  * Node Package Modules
  */
 const process_1 = require("process");
+const Colors = __importStar(require("colors"));
+const colors = Colors;
 /**
  * CLI Library Modules
  */
-const lib_1 = require("../lib");
+const lib = __importStar(require("../lib"));
 //
 // START CLI Script
 //
@@ -23,33 +44,38 @@ if (process_1.argv.length > 3) {
 console.log(`DGWNU - Fuseki Service - ${command} ${serverArgs ? serverArgs.join(' ') : ''}`);
 switch (command) {
     case 'run': {
-        const output = lib_1.runServer(serverArgs);
+        const output = lib.runServer(serverArgs);
         console.log(output);
         break;
     }
     case 'start': {
-        lib_1.startServer(serverArgs).subscribe({
+        lib.startServer(serverArgs).subscribe({
             next: () => console.log('Server Started!'),
             error: (err) => console.error(err)
         });
         break;
     }
     case 'restart': {
-        lib_1.restartServer().subscribe({
+        lib.restartServer().subscribe({
             next: () => console.log('Server Restarted!'),
             error: (err) => console.error(err)
         });
         break;
     }
     case 'stop': {
-        lib_1.stopServer().subscribe({
+        lib.stopServer().subscribe({
             next: () => console.log('Server Stopped!'),
             error: (err) => console.error(err)
         });
         break;
     }
     default: {
-        console.error(`Unknown Command: ${command}`);
+        console.log('\nNot a valid parameter command line argument. Please use:\n');
+        console.log('fuseki-service run          Run Fuseki Server in a Terminal Session (terminated after closing)'.red);
+        console.log('fuseki-service run --help   For all available Fuseki Server Options');
+        console.log('fuseki-service start        Start Fuseki Server as a permanent PM2 deamon');
+        console.log('fuseki-service stop         Stop Fuseki Server as a permanent PM2 deamon');
+        console.log('fuseki-service restart      Restart Fuseki Server as a permanent PM2 deamon');
         break;
     }
 }
--- src/bin/fuseki-service-cli.ts
+++ src/bin/fuseki-service-cli.ts
@@ -8,11 +8,18 @@
  * Node Package Modules
  */
 import { argv } from 'process';
+import * as Colors from 'colors';
 
 /**
  * CLI Library Modules
  */
-import { runServer, startServer, restartServer, stopServer } from '../lib'
+import * as lib from '../lib'
+
+/**
+ * colors constant that is used for console.log()
+ * Based on <https://www.voidcanvas.com/make-console-log-output-colorful-and-stylish-in-browser-node/>
+ */
+const colors = Colors;
 
 //
 // START CLI Script
@@ -24,18 +31,18 @@ if (argv.length > 3) {
     serverArgs = argv.slice(3, argv.length);
 }
 
-console.log(`DGWNU - Fuseki Service - ${command} ${serverArgs ? serverArgs.join(' ') : ''}`);
+console.log(`DGWNU - Fuseki Service - ${command} ${serverArgs ? serverArgs.join(' ') : ''}`.bgRed.yellow.bold);
 
 switch (command) {
 
     case 'run': {
-        const output = runServer(serverArgs);
+        const output = lib.runServer(serverArgs);
         console.log(output);
         break;
     }
 
     case 'start': {
-        startServer(serverArgs).subscribe({
+        lib.startServer(serverArgs).subscribe({
             next: () => console.log('Server Started!'),
             error: (err) => console.error(err)
         });
@@ -43,7 +50,7 @@ switch (command) {
     }
 
     case 'restart': {
-        restartServer().subscribe({
+        lib.restartServer().subscribe({
             next: () => console.log('Server Restarted!'),
             error: (err) => console.error(err)
         });
@@ -51,7 +58,7 @@ switch (command) {
     }
 
     case 'stop': {
-        stopServer().subscribe({
+        lib.stopServer().subscribe({
             next: () => console.log('Server Stopped!'),
             error: (err) => console.error(err)
         });
@@ -59,7 +66,12 @@ switch (command) {
     }
 
     default: {
-        console.error(`Unknown Command: ${command}`);
+        console.log('\nNot a valid parameter command line argument. Please use:\n'.bold);
+        console.log('fuseki-service run          Run Fuseki Server in a Terminal Session (terminated after closing)'.red);
+        console.log('fuseki-service run --help   For all available Fuseki Server Options'.green);
+        console.log('fuseki-service start        Start Fuseki Server as a permanent PM2 deamon'.blue);
+        console.log('fuseki-service stop         Stop Fuseki Server as a permanent PM2 deamon'.blue);
+        console.log('fuseki-service restart      Restart Fuseki Server as a permanent PM2 deamon'.blue);
         break;
     }
 }
